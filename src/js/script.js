1df5fe6d8f61f9f26b35608b1cc838686be7690e
Edmund <pieniu1511@gmail.com>
2023-01-09 15:25:00 +0100

    trying to solve video problem on mobile 3

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,10 @@ const burgerBtn = document.querySelector('.burger-icon');
 const hiddenMenu = document.querySelector('.hidden-menu');
 const closeBtn = document.querySelector('.close-menu');
 const cards = document.querySelectorAll('.card');
-const heroBottom = document.querySelector('.hero-bottom');
-const heroText = heroBottom.querySelector('p');
-const leftButton = document.querySelector('.hero-btn-left');
-const rightButton = document.querySelector('.hero-btn-right');
+// const heroBottom = document.querySelector('.hero-bottom');
+// const heroText = heroBottom.querySelector('p');
+// const leftButton = document.querySelector('.hero-btn-left');
+// const rightButton = document.querySelector('.hero-btn-right');
 
 cards.forEach(card => {
 	const btn = card.querySelector('.plus-btn');
@@ -16,16 +16,16 @@ cards.forEach(card => {
 	});
 });
 
-let currentIndex = 0;
+// let currentIndex = 0;
 
-const classes = ['first-img', 'second-img', 'third-img', 'fourth-img'];
+// const classes = ['first-img', 'second-img', 'third-img', 'fourth-img'];
 
-let texts = [
-	'Ekologiczne rozwiązania dla przyszłych pokoleń',
-	'Powiedz GOODBYE rachunkom za energię z naszymi panelami słonecznymi',
-	'Oszczędzaj pieniądze i środowisko dzięki odnawialnej energii',
-	'Tańsza energia dla Twojego domu i firmy',
-];
+// let texts = [
+// 	'Ekologiczne rozwiązania dla przyszłych pokoleń',
+// 	'Powiedz GOODBYE rachunkom za energię z naszymi panelami słonecznymi',
+// 	'Oszczędzaj pieniądze i środowisko dzięki odnawialnej energii',
+// 	'Tańsza energia dla Twojego domu i firmy',
+// ];
 
 const showMenu = () => {
 	hiddenMenu.style.transform = 'translateX(0)';
@@ -35,31 +35,31 @@ const closeMenu = () => {
 	hiddenMenu.style.transform = 'translateX(100%)';
 };
 
-function updateImage() {
-	currentIndex = (currentIndex + 1) % classes.length;
-	if (currentIndex === 0) {
-		heroBottom.classList.replace(classes[classes.length - 1], classes[currentIndex]);
-	} else {
-		heroBottom.classList.replace(classes[currentIndex - 1], classes[currentIndex]);
-	}
-	heroText.textContent = texts[currentIndex];
-}
+// function updateImage() {
+// 	currentIndex = (currentIndex + 1) % classes.length;
+// 	if (currentIndex === 0) {
+// 		heroBottom.classList.replace(classes[classes.length - 1], classes[currentIndex]);
+// 	} else {
+// 		heroBottom.classList.replace(classes[currentIndex - 1], classes[currentIndex]);
+// 	}
+// 	heroText.textContent = texts[currentIndex];
+// }
 
-setInterval(updateImage, 10000);
+// setInterval(updateImage, 10000);
 
 burgerBtn.addEventListener('click', showMenu);
 closeBtn.addEventListener('click', closeMenu);
-leftButton.addEventListener('click', function () {
-	currentIndex = (currentIndex - 1 + classes.length) % classes.length;
-	heroBottom.classList.replace(classes[currentIndex + 1], classes[currentIndex]);
-	heroText.textContent = texts[currentIndex];
-});
-rightButton.addEventListener('click', function () {
-	currentIndex = (currentIndex + 1) % classes.length;
-	if (currentIndex === 0) {
-		heroBottom.classList.replace(classes[classes.length - 1], classes[currentIndex]);
-	} else {
-		heroBottom.classList.replace(classes[currentIndex - 1], classes[currentIndex]);
-	}
-	heroText.textContent = texts[currentIndex];
-});
+// leftButton.addEventListener('click', function () {
+// 	currentIndex = (currentIndex - 1 + classes.length) % classes.length;
+// 	heroBottom.classList.replace(classes[currentIndex + 1], classes[currentIndex]);
+// 	heroText.textContent = texts[currentIndex];
+// });
+// rightButton.addEventListener('click', function () {
+// 	currentIndex = (currentIndex + 1) % classes.length;
+// 	if (currentIndex === 0) {
+// 		heroBottom.classList.replace(classes[classes.length - 1], classes[currentIndex]);
+// 	} else {
+// 		heroBottom.classList.replace(classes[currentIndex - 1], classes[currentIndex]);
+// 	}
+// 	heroText.textContent = texts[currentIndex];
+// });
