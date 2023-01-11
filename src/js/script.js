9851221387ba429c462a32ce0f6beed11cf342ed
Edmund <pieniu1511@gmail.com>
2023-01-11 19:40:48 +0100

    added tiles on hero

--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,14 @@ const cards = document.querySelectorAll('.card');
 const dropdown = document.querySelector('.nav-item--dropdown');
 const offerBtn = document.querySelector('.offer');
 const offerOptions = document.querySelector('.offer-options');
+const body = document.querySelector('body');
 
 const dropdownMenu = document.createElement('div');
 dropdownMenu.classList.add('dropdown-menu');
 dropdownMenu.innerHTML =
-	'<a href="#">Instalacje fotowoltaiczne</a><a href="#">Pompy ciepła</a><a href="#">Tania energia</a><a href="#">Dotacje</a>';
+	'<a href="installations.html">Instalacje fotowoltaiczne</a><a href="heatpumps.html">Pompy ciepła</a><a href="cheapenergy.html">Tania energia</a><a href="grants.html">Dotacje</a>';
 dropdown.appendChild(dropdownMenu);
+
 
 cards.forEach(card => {
 	const btn = card.querySelector('.plus-btn');
@@ -23,16 +25,18 @@ cards.forEach(card => {
 
 const showMenu = () => {
 	hiddenMenu.style.transform = 'translateX(0)';
+	body.style.overflow = 'hidden';
 };
 
 const closeMenu = () => {
 	hiddenMenu.style.transform = 'translateX(100%)';
-    offerOptions.classList.remove('show-options')
+	offerOptions.classList.remove('show-options');
+    body.style.overflow = 'auto';
 };
 
 burgerBtn.addEventListener('click', showMenu);
 closeBtn.addEventListener('click', closeMenu);
 offerBtn.addEventListener('click', function (e) {
-    e.preventDefault();
+	e.preventDefault();
 	offerOptions.classList.toggle('show-options');
 });
